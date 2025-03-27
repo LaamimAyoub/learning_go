bd17bd7d3c31e82f2debb9b9463643e99b7f711b
Ayb <ayoublaamim1996@gmail.com>
2025-03-27 23:04:18 +0000

    added a more complex test

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -1,6 +1,9 @@
 package reflection
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
 	expected := "Chriss"
@@ -22,6 +25,37 @@ func TestWalk(t *testing.T) {
 
 	if got[0] != expected {
 		t.Errorf("Wrong func call want %s, got %s", expected, got[0])
+	}
+
+}
+
+func TestWalkV2(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one field",
+			struct {
+				Name string
+			}{"Chriss"},
+			[]string{"Chriss"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			Walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("Failed test")
+			}
+
+		})
 	}
 
 }
