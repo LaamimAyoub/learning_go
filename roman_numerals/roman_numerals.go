676b910a19885ef8e4c6580a7561a8b15718a5d9
Ayb <ayoublaamim1996@gmail.com>
2025-05-19 21:03:32 +0100

    Finished roman numbers converter

--- roman_numerals/roman_numerals.go
+++ roman_numerals/roman_numerals.go
@@ -1,18 +1,40 @@
 package romannumerals
 
-func multiply_letter(number int, letter rune) string {
-	str := ""
-	for i := 0; i < number; i++ {
-		str = str + string(letter)
-	}
-
-	return str
+var mapNumbers = map[int]string{
+	1:    "I",
+	4:    "IV",
+	5:    "V",
+	9:    "IX",
+	10:   "X",
+	40:   "XL",
+	50:   "L",
+	90:   "XC",
+	100:  "C",
+	400:  "CD",
+	500:  "D",
+	900:  "DM",
+	1000: "M",
 }
+
+var numbers = []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
 
 func ConvertToRoman(arabic int) string {
 	str := ""
-	if arabic < 4 {
-		str += multiply_letter(arabic, 'I')
+	var temp int
+	var res int = arabic
+
+	for _, i := range numbers {
+
+		if res >= i {
+			temp = res / i
+
+			res = res - temp*i
+
+			for j := 0; j < temp; j++ {
+				str += mapNumbers[i]
+			}
+
+		}
 	}
 
 	return str
--- roman_numerals/roman_numerals_test.go
+++ roman_numerals/roman_numerals_test.go
@@ -15,4 +15,7 @@ func TestRomanNumerals(t *testing.T) {
 	check_value(t, 1, "I")
 	check_value(t, 2, "II")
 	check_value(t, 3, "III")
+	check_value(t, 1001, "MI")
+	check_value(t, 1003, "MIII")
+	check_value(t, 801, "DCCCI")
 }
